bb28e070788d58c62ddeda257314781131f51f98
nitiphum <65030130@kmitl.ac.th>
2024-03-24 23:04:54 +0700

    commitex1

--- Lab12_Ex01/obj/Debug/net8.0/Lab12_Ex01.AssemblyInfo.cs
+++ Lab12_Ex01/obj/Debug/net8.0/Lab12_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab12_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0b9b667152835cd250637c7e73987a01aa8227")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a2a31eb7fa2257b4bd1d983afbd951c6049615")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab12_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab12_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
